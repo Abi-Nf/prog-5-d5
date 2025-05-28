31e5d69804ca7b45e2763e95141083671a8c3363
Abi-Nf <hei.abigail.3@gmail.com>
2025-05-28 09:34:49 +0300

    chore: simulate machine preparation for 5s

---
+++ src/main/kotlin/com/project/clean/CoffeeRequest.kt
@@ -0,0 +1,34 @@
+package com.project.clean
+
+import java.util.function.Supplier
+
+class CoffeeRequest internal constructor(
+    money: Float,
+    private val requestADrink: RequestADrink
+) {
+    companion object {
+        private const val PRICE_FOR_COFFEE = 2000f
+
+        fun simulatePreparation() {
+            Thread.sleep(5000)
+        }
+    }
+
+    var money: Float = money
+        private set
+
+    init {
+        if (money < PRICE_FOR_COFFEE) {
+            throw CoffeeRequestException("The price for coffee is $PRICE_FOR_COFFEE")
+        }
+    }
+
+    fun chooseACoffee(choice: CoffeeChoice): Supplier<CoffeeDrink> {
+        requestADrink.request(choice)
+
+        return Supplier<CoffeeDrink> {
+            simulatePreparation()
+            CoffeeDrink
+        }
+    }
+}
