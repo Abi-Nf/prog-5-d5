41239657848ffb5793c850b07628a226c2b364e3
Abi-Nf <hei.abigail.3@gmail.com>
2025-05-28 09:36:56 +0300

    chore: only a coffee drink request can call request()

---
+++ src/main/kotlin/com/project/clean/CoffeeMachine.kt
@@ -0,0 +1,41 @@
+package com.project.clean
+
+class CoffeeMachine {
+    private var coffeeCups: Int = 0
+    private var currentWaterCapacity: Float = 0f
+    private var currentCoffeeMold: Float = 0f
+
+    fun requestACoffee(money: Float): CoffeeRequest {
+        return CoffeeRequest(money, ::request)
+    }
+
+    fun addWater(capacity: Float) {
+        this.currentWaterCapacity += capacity
+    }
+
+    fun addCoffeeMold(quantity: Float) {
+        this.currentCoffeeMold += quantity
+    }
+
+    fun addCups(number: Int) {
+        this.coffeeCups += number
+    }
+
+    private fun request(variety: CoffeeVariety) {
+        if ((currentWaterCapacity - variety.waterQuantity) <= 0) {
+            throw CoffeeMachineException("Not enough water for a drink")
+        }
+
+        if ((currentCoffeeMold - variety.coffeeMoldQuantity) <= 0) {
+            throw CoffeeMachineException("Not enough coffee mold")
+        }
+
+        if (coffeeCups == 0) {
+            throw CoffeeMachineException("Not enough cup for your drink, sorry !")
+        }
+
+        currentWaterCapacity -= variety.waterQuantity
+        currentCoffeeMold -= variety.coffeeMoldQuantity
+        coffeeCups--
+    }
+}
